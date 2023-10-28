c9827d9b6c38fc4d12903993a6fd7e5f16bdd014
alejandrocalderonhernandez <alejandro950825@gmail.com>
2023-10-28 13:01:01 -0600

    refactor posgresql

---
+++ postgresql/sql/create_schema.sql
@@ -0,0 +1,6 @@
+create table customers(
+    id    bigserial primary key,
+    email varchar(70)  not null,
+    pwd   varchar(500) not null
+);
+
---
+++ postgresql/sql/data.sql
@@ -0,0 +1,5 @@
+insert into customers (email, pwd) values
+    ('account@debuggeandoieas.com', 'to_be_encoded'),
+    ('cards@debuggeandoieas.com', 'to_be_encoded'),
+    ('loans@debuggeandoieas.com', 'to_be_encoded'),
+    ('balance@debuggeandoieas.com', 'to_be_encoded');
